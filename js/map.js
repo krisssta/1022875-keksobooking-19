2bd667c3b66b2560a724b711e88c9e3114e4ec2f
krisssta <krisssta69@gmail.com>
2020-02-25 21:49:46 +0700

    Личный проект: надо подкачаться (часть 1)

--- js/map.js
+++ js/map.js
@@ -2,22 +2,38 @@
 
 (function () {
 
-  var offers = window.data.offers;
+  // var offers = window.data.offers;
   var map = document.querySelector('.map');
   var mapPins = document.querySelector('.map__pins');
   var fragment = document.createDocumentFragment();
 
   // generate map pins elements from offers
-  for (var i = 0; i < offers.length; i++) {
-    var pinElement = window.pin.createPin(offers[i]);
-    fragment.appendChild(pinElement);
-  }
+  var successHandler = function (pins) {
 
-  // paste map pins to map
-  mapPins.appendChild(fragment);
+    for (var i = 0; i < pins.length; i++) {
+      fragment.appendChild(window.pin.createPin(pins[i]));
+    }
 
-  // create card element for current offer & render it in map
-  var cardElement = window.card.createCard(offers[0]);
-  map.appendChild(cardElement);
+    mapPins.appendChild(fragment);
+    // create card element for current offer & render it in map
+    var cardElement = window.card.createCard(pins[0]);
+    map.appendChild(cardElement);
+  };
+
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.load(successHandler, errorHandler);
 
 })();
+
+
